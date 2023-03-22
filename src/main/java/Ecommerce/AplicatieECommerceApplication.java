da7b798382349a217d0dcfda70ad210a8b565717
Cristina <ionelenacristina0@gmail.com>
2023-03-22 23:20:58 +0200

    Init project

---
+++ src/main/java/Ecommerce/AplicatieECommerceApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicatieECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicatieECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ecommerce/controller/AplicatieECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecommerce.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicatieECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
