90277ea0b603231916fd5ac60ac862eeaef9a21c
Cristina <ionelenacristina0@gmail.com>
2023-04-12 00:54:30 +0300

    Add authentication

---
+++ src/main/java/Ecommerce/security/config/JwtService.java
@@ -0,0 +1,64 @@
+package Ecommerce.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    //TODO -- put the key in properties file
+    private static final String SECRET_KEY="743777217A25432A462D4A404E635266556A586E3272357538782F413F442847";
+    public String extractUsername(String jwtToken) {
+        return extractClaim(jwtToken,Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String jwt){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+24*60*1000))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails){
+        final String username = extractUsername(jwtToken);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken));
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken){
+        return extractClaim(jwtToken,Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ src/main/java/Ecommerce/security/user/Role.java
@@ -0,0 +1,6 @@
+package Ecommerce.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
